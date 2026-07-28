4c1f2db4fee733fe6836f77fce440ec2a09e5063
ushanlokuge <ushanlokuge@gmail.com>
2026-07-28 23:27:33 +1000

    Refactor player rendering HTML structure

--- js/pages/players.js
+++ js/pages/players.js
@@ -1,23 +1,22 @@
 export function renderPlayers() {
-
     return `
-        <h2>👥 Players</h2>
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h2>👥 Players</h2>
 
-        <button class="btn btn-success mb-3">
-            + Add Player
-        </button>
+            <button class="btn btn-success" id="addPlayerBtn">
+                + Add Player
+            </button>
+        </div>
 
         <table class="table table-dark table-striped">
 
             <thead>
-
                 <tr>
                     <th>Name</th>
                     <th>Group</th>
                     <th>Marches</th>
                     <th></th>
                 </tr>
-
             </thead>
 
             <tbody id="playerTable">
@@ -26,5 +25,4 @@ export function renderPlayers() {
 
         </table>
     `;
-
 }
