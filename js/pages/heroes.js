aab2ebfc8c5ddb8a1cf264e4feed25a0e1880f7d
ushanlokuge <ushanlokuge@gmail.com>
2026-07-28 23:24:55 +1000

    Add renderHeroes function to display heroes

---
+++ js/pages/heroes.js
@@ -0,0 +1,13 @@
+export function renderHeroes(){
+
+    return `
+        <h2>🛡 Heroes</h2>
+
+        <button class="btn btn-success mb-3">
+            + Add Hero
+        </button>
+
+        <div id="heroTable"></div>
+    `;
+
+}
