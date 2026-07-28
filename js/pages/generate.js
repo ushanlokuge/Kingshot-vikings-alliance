ea06d298c85f8c7c75fa6cd462df79155fc64d13
ushanlokuge <ushanlokuge@gmail.com>
2026-07-28 23:25:41 +1000

    Add renderGenerate function for Viking assignments

---
+++ js/pages/generate.js
@@ -0,0 +1,16 @@
+export function renderGenerate(){
+
+    return `
+        <h2>⚔ Generate Viking Assignments</h2>
+
+        <button id="generateBtn"
+            class="btn btn-danger btn-lg">
+
+            Generate
+
+        </button>
+
+        <div class="mt-4" id="results"></div>
+    `;
+
+}
