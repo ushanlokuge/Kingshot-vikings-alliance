7792649a434e5afbcbd63e1d17d3b024ec40fdbf
ushanlokuge <ushanlokuge@gmail.com>
2026-07-28 23:25:19 +1000

    Add renderGroups function to display groups section

---
+++ js/pages/groups.js
@@ -0,0 +1,13 @@
+export function renderGroups(){
+
+    return `
+        <h2>📂 Groups</h2>
+
+        <button class="btn btn-success mb-3">
+            + Create Group
+        </button>
+
+        <div id="groupTable"></div>
+    `;
+
+}
