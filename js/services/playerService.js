a62a3f45ca67a9ba925489fb9096b3bb66aaba93
ushanlokuge <ushanlokuge@gmail.com>
2026-07-28 23:49:49 +1000

    Implement player service with CRUD operations

---
+++ js/services/playerService.js
@@ -0,0 +1,49 @@
+import { db } from "../firebase.js";
+
+import {
+    collection,
+    getDocs,
+    addDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+    query,
+    orderBy
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const playersRef = collection(db, "players");
+
+export async function getPlayers() {
+
+    const q = query(
+        playersRef,
+        orderBy("group"),
+        orderBy("name")
+    );
+
+    const snapshot = await getDocs(q);
+
+    return snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+
+}
+
+export async function addPlayer(player){
+
+    return await addDoc(playersRef, player);
+
+}
+
+export async function deletePlayer(id){
+
+    return await deleteDoc(doc(db,"players",id));
+
+}
+
+export async function editPlayer(id,player){
+
+    return await updateDoc(doc(db,"players",id),player);
+
+}
